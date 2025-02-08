45e9ab339d03b271e511c41d7c04a6f89f68bb7c
Erick Christian <erickchristian48@gmail.com>
2025-02-08 09:02:49 +0700

    fix: Use correct env variable name for Discord client ID

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,7 +1,7 @@
 import { getEnv } from "@echristian/env"
 
 const DISCORD_TOKEN = getEnv("DISCORD_TOKEN")
-const DISCORD_CLIENT_ID = getEnv("CLIENT_ID")
+const DISCORD_CLIENT_ID = getEnv("DISCORD_CLIENT_ID")
 
 export const ENV = {
   DISCORD_TOKEN,
