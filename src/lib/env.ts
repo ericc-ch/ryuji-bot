58e0a7ed4ee7f6ae3b365b65a4f6b51d62c4dcfd
Erick Christian <erickchristian48@gmail.com>
2025-02-10 00:06:52 +0700

    feat: Add GEMINI_API_KEY to environment variables

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -3,7 +3,11 @@ import { getEnv } from "@echristian/env"
 const DISCORD_TOKEN = getEnv("DISCORD_TOKEN")
 const DISCORD_CLIENT_ID = getEnv("DISCORD_CLIENT_ID")
 
+const GEMINI_API_KEY = getEnv("GEMINI_API_KEY")
+
 export const ENV = {
   DISCORD_TOKEN,
   DISCORD_CLIENT_ID,
+
+  GEMINI_API_KEY,
 }
